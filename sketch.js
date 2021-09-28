5daf35bcffd51147e0984e05318ac39f24abc70e
zulaikamunseena <75322587+zulaikamunseena@users.noreply.github.com>
2021-09-28 06:40:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var trex ;// trex_running, trex_collided;
+// var ground, invisibleGround, groundImage;
+
+ function preload(){
+
+
+
+
+
+ trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  }
+
+ function setup() {
+ createCanvas(600, 400);
+  
+//   //create a trex sprite
+  trex = createSprite(50,180,20,50);
+ trex.addAnimation( "running", trex_running);
+  
+//   //adding scale and position to trex
+//   trex.scale = 0.5;
+//   trex.x = 50
+  
+//   //create ground sprite
+//   ground = createSprite(200,180,400,20);
+ 
+ }
+
+  function draw() {
+ background("orange");
+ 
+//   //jumping the trex on space key press
+   if(keyDown("space")) {
+   trex.velocityY = -10;
+  }
+  
+   trex.velocityY = trex.velocityY + 0.8
+  
+ 
+//  //stop trex from falling down 
+//   trex.collide(ground);
+  drawSprites();
+ }
